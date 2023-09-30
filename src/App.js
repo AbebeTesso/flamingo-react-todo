9923074c6d6df06c74431c79570be38d73c30739
Abebe_Tesso <abetesso@gmail.com>
2023-09-29 19:15:45 -0700

    lesson1added

--- src/App.js
+++ src/App.js
@@ -1,16 +1,32 @@
 import React from 'react';
-
+const todoList = [
+  {title: "Code The Dream",
+  id: 1,
+  lesson: 1.1,
+},
+  {title: "Flamingo React Class",
+  id: 2,
+  lesson: 1.1,
+},
+  {title: "Complete The Assignment",
+  id: 3,
+  lesson: 1.1,
+},
+];
 function App() {
   return (
-    <div style={{ textAlign: 'center' }}>
-      <header>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-          Learn React
-        </a>
-      </header>
+    <div> 
+      <h1>Todo List</h1>
+      <ul>
+        {todoList.map(function(item){
+          return <li key={item.id}>
+                  <a href={item.id}> {item.title}
+                  <span> {item.id} </span>
+                  <span> {item.lesson} </span>
+                  </a>
+                </li>
+        })}
+      </ul>
     </div>
   );
 }
